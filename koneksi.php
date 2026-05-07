ebb0dd38ffb194d188f328b184928064b0acb9f5
Juna1d1 <kucinggot215@gmail.com>
2026-05-07 12:04:23 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,17 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
-$port = $_ENV['MYSQLPORT'];
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+if (!$koneksi) {
+    die(json_encode([
+        "status" => "error",
+        "message" => mysqli_connect_error()
+    ]));
 }
 ?>
